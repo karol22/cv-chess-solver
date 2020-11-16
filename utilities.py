73ae307e917ec361d1c3b2ff5e676d9f03153b71
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-11-16 15:27:06 -0600

    Utilities updated
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

--- utilities.py
+++ utilities.py
@@ -1,9 +1,21 @@
-def from_board_to_fne(board):
-    def get_piece(piece):
-        return {
-            ".": False
-        }.get(piece, piece)
+def get_piece(piece):
+    return  {
+        0: 'afil_azul',
+        1: 'afil_blanco',
+        2: 'caballo_azul',
+        3: 'caballo_blanco',
+        4: 'peon_azul',
+        5: 'peon_blanco',
+        6: 'reina_azul',
+        7: 'reina_blanco',
+        8: 'rey_azul',
+        9: 'rey_blanco',
+        10: 'torre_azul',
+        12: 'torre_blanco'
+    }.get(piece, piece)
 
+
+def from_board_to_fne(board):
     fne_board = []
     # 'for loop' until the shape is defined
     for row in board.splitlines():
